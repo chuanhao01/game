16eb51da8a2188e22d7d78b61d982bb1fc7d4063
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2024-10-05 22:33:33 +0800

    [WIP] nah

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+// System
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
+// Entity
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name(String::from("Bob"))));
+    commands.spawn((Person, Name(String::from("Merry"))));
+    commands.spawn((Person, Name(String::from("Jess"))));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Bob" {
+            name.0 = String::from("Mega Bob");
+            return;
+        }
+    }
+}
+
+fn hello_world() {
+    println!("Hello World!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (update_people, greet_people).chain())
+        .run();
 }
